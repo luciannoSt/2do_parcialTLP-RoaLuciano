ee72e8c9c7a53833a8977881097442c8e27f5842
luciannoSt <luciannotwt@gmail.com>
2024-06-27 14:41:24 -0300

    se hace la estructura get de traer un libro por id

--- app.js
+++ app.js
@@ -4,28 +4,31 @@ const app = express();
 
 app.use(express.json());
 
-let libros = [
-    libros:[
-        {
-            id: 1,
-            titulo: 'El Señor de los Anillos',
-            autor: 'J.R.R. Tolkien',
-            editorial: 'Panini Books',
-            anio: 1954
-        },
-        {
-            id: 2,
-            titulo: 'El Quijote de la Mancha',
-            autor: 'Miguel de Cervantes',
-            editorial: 'Emece',
-            anio: 1813
-        },
-        {
-            id: 3,
-            titulo: 'La Divina Comedia',
-            autor: 'Miguel de Cervantes',
-            editorial: 'Emece',
-            anio: 1605
-        }
-    ]
+let libros=[
+    {id:1, titulo:'primer libro', autor:'Roa Luciano', year:2004},
+    {id:2, titulo:'La Divina Comedia', autor:'marcelo ortega', year:2002},
+    {id:3, titulo:'El Quijote', autor:'Ortiz Emilio', year:2005},
 ]
+
+//get que sirve para traer todos los libros del array
+
+app.get('/libros', (req,res) => {
+    res.json(libros);
+});
+
+//get para traer un libro segun su id
+
+app.get('/libros/:id', (req,res) => {
+    const id= req.params.id;
+    const libro = libros.find(libro => libro.id === parseInt(id));
+    if(!libro){
+        return res.status(404).json({mensaje:`Libro con id ${id} no encontrado`});
+    }
+    res.json(libro);
+});
+
+//
+
+
+app.listen(3000,(req,res) => {
+    console.log('servidor corriendo en el puerto 3000');});
